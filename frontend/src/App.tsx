1e8b98db4defbe2a8d0fa4221311e4822ac714a2
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-10-27 18:04:23 +0100

    Content definition

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,236 @@ import './index.css';
 
 function App() {
   return (
-    <h1>Sara Vertuan Profile</h1>
+    <>
+      <h1>Sara Vertuan Profile</h1>
+      <h1>INTERFACE</h1>
+
+      <h1>HEADER</h1>
+      <h3>Idioma</h3>
+      <h5>Português</h5>
+      <h5>Inglês</h5>
+      <h5>Italiano</h5>
+      <h3>Tema</h3>
+      <h5>Light</h5>
+      <h5>Dark</h5>
+
+      <h1>MENU</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h4>Sara Vertuan</h4>
+      <h5>Capas de livros e Diagramatização</h5>
+      <h6>Início</h6>
+      <h6>Sobre Mim</h6>
+      <h6>Depoimentos</h6>
+      <h6>Galeria</h6>
+      <h6>Contato</h6>
+
+      <h1>INÍCIO</h1>
+      <h3>AQUI VAI UMA FOTO</h3>
+      <h3>AQUI VAI UMA FOTO</h3>
+      <h3>AQUI VAI UMA FOTO</h3>
+      <h3>AQUI VAI UMA FOTO</h3>
+      <h3>AQUI VAI UMA FOTO</h3>
+      <h3>AQUI VAI UMA FOTO</h3>
+      <h3>AQUI VAI UMA FOTO</h3>
+      <h3>AQUI VAI UMA FOTO</h3>
+      <h3>AQUI VAI UMA FOTO</h3>
+      <h3>AQUI VAI UMA FOTO</h3>
+      <h3>AQUI VAI UMA FOTO</h3>
+      <h3>AQUI VAI UMA FOTO</h3>
+      <h3>AQUI VAI UMA FOTO</h3>
+      <h3>AQUI VAI UMA FOTO</h3>
+      <h3>AQUI VAI UMA FOTO</h3>
+      <h3>AQUI VAI UMA FOTO</h3>
+      <h3>AQUI VAI UMA FOTO</h3>
+      <h3>AQUI VAI UMA FOTO</h3>
+      <h3>AQUI VAI UMA FOTO</h3>
+      <h3>AQUI VAI UMA FOTO</h3>
+      <h3>AQUI VAI UMA FOTO</h3>
+      <h3>AQUI VAI UMA FOTO</h3>
+      <h3>AQUI VAI UMA FOTO</h3>
+      <h3>AQUI VAI UMA FOTO</h3>
+      <p>
+        PORQUE DEVO INVESTIR NUMA CAPA PROFISSIONAL?
+
+        Uma capa, seja ela física ou digital, é o elemento mais importante de um
+        livro. Além de ser o primeiro contato do leitor com a obra, protege o
+        miolo do livro e representa um percentual superior a 70% nas vendas de
+        um livro.
+
+        É necessário realizar uma pesquisa sobre tendências de design,
+        público-alvo, tempo histórico, etc; e tambem levar em conta os gostos do
+        autor para assim chegar a uma capa de qualidade e que atraia o público
+        podendo tranformar o livro em um objeto de coleção na estante.
+      </p>
+      <p>
+        PERCHÉ INVESTIRE IN UNA COPERTINA PROFESSIONALE?
+
+        La copertina, sia essa fisica o digitale, è l'elemento più importante di
+        un libro. Oltre a essere il primo contatto del lettore con l'opera,
+        protegge il nucleo del libro e rappresenta oltre il 70% delle vendite
+        di un libro.
+
+        É necessario effettuare ricerche sulle tendenze del design, sul pubblico
+        di riferimento, sul periodo storico, ecc. e tenere conto anche dei gusti
+        dell'autore per ottenere una copertina di qualità che possa attrarre il
+        pubblico e trasformare il libro in un oggetto da collezione sugli
+        scaffali delle librerie.
+      </p>
+      <p>
+        WHY SHOULD I INVEST IN A PROFESSIONAL COVER?
+
+        A cover, whether physical or digital, is the most important element of a
+        book. Besides being the reader's first contact with the work, it
+        protects the book's core and represents more than 70% of a book's sales.
+
+        To make a high quality cover it is necessary to carry out researches
+        based on design trends, target audience, historical time, etc. Moreover,
+        it is also important to take into account the author's tastes.
+        In this way the cover will have more chances to attract the public,
+        making the book a collector's item on the bookshelf.
+      </p>
+
+      <h1>SOBRE MIM</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <p>
+        Hello,
+        Meu nome é Sara Vertuan, sou Designer Editorial, desenvolvo Capas e
+        Diagramações de livros impressos e Ebooks, sou formada em Publicidade e
+        Propaganda. Sou freelancer para autores e editoras, meu amor pelos
+        livros faz com que o meu maior desejo seja o desenvolvimento de uma capa
+        linda que atraia os leitores e agrade meu cliente, sempre aperfeiçoando
+        meu trabalho para garantir a qualidade e melhorar cada dia mais.
+      </p>
+      <p>
+        Hello,
+        Mi chiamo Sara Vertuan, sono Editorial Designer, sviluppo copertine e
+        layout per libri stampati ed Ebook, sono laureata in Pubblicità e
+        Propaganda. Sono una freelance per autori ed editori, il mio amore per i
+        libri fa sì che il mio più grande desiderio sia lo sviluppo di una bella
+        copertina che attiri i lettori e faccia piacere al mio cliente,
+        migliorando sempre il mio lavoro per garantire la qualità e migliorare
+        ogni giorno.
+      </p>
+      <p>
+        My name is Sara Vertuan, I am Editorial Designer, I develop Covers and
+        Layouts for printed books and Ebooks, I am graduated in Advertising and
+        Publicity. I am a freelancer for authors and publishers, my love for
+        books makes my greatest desire is the development of a beautiful cover
+        that attracts readers and please my client, always improving my work to
+        ensure quality and improve every day.
+      </p>
+      <h5>Whatsapp</h5>
+      <h6>+5532261228</h6>
+      <h5>E-mail</h5>
+      <h6>saracvertuan@gmail.com</h6>
+      <h5>Facebook</h5>
+      <h6>facebook.com/designsaravertuan</h6>
+      <h5>Instagram</h5>
+      <h6>instagram.com/designersaravertuan</h6>
+      <h5>Behancé</h5>
+      <h6>behance.net/saravertuan</h6>
+      <h5>Linkedin</h5>
+      <h6>linkedin.com/in/saracvertuan/</h6>
+
+      <h1>DEPOIMENTOS</h1>
+      <h3>Gabriela Maya</h3>
+      <h5>Autora</h5>
+      <p>
+        Sara Vertuan é uma profissional sensível e talentosa, consegue captar o
+        desejo do autor e torná-lo ainda mais especial. Atenciosa, gentil e doce
+        é também alguém com quem podemos contar para cumprir prazos estipulados
+        previamente. Sua arte surpreende e dialoga com a premissa das obras
+        cujas capas ela assina brilhantemente. Não há uma pessoa que segure meu
+        livro, Uma Vida Para Anne, sem comentar sobre a beleza da capa. Mérito
+        da Sara!
+      </p>
+
+      <h3>Bettina Winkler</h3>
+      <h5>Autora</h5>
+      <p>
+        Sara é uma excelente profissional, que se esforça ao máximo para
+        entender os desejos do cliente e atingir o melhor resultado possível.
+        Fiz a capa do meu livro de fantasia com ela e recebo elogios o tempo
+        todo, não poderia ter escolhido uma capista melhor.
+      </p>
+
+      <h3>Rodrigo Seixas</h3>
+      <h5>Autor e Quadrinista</h5>
+      <p>
+        Sara é uma das profissionais mais completas do mercado. Possui uma série
+        de atributos que torna seu trabalho único: senso estético, velocidade e
+        GOSTO POR LEITURA! Tudo isso facilita demais o diálogo com o autor e
+        tradução de suas ideias capas incríveis! Recomendo DEMAIS!!!
+      </p>
+
+      <h3>Ana Maria Duarte</h3>
+      <h5>Autora</h5>
+      <p>
+        O trabalho da Sara se mostrou melhor do que eu imaginava. A qualidade
+        do serviço foi excelente e ela soube captar exatamente a essência do meu
+        livro em todos os detalhes, na capa e na diagramação. Foi incrível poder
+        ver uma ideia que estava apenas na minha cabeça se materializando de
+        maneira única e tão especial!!
+      </p>
+
+      <h3>Aurélia Cruz</h3>
+      <h5>Autora</h5>
+      <p>
+        Precisava de uma capa mas já tinha passado por várias sugestões e
+        nenhuma conseguia causar o impacto que eu procurava. Até que a querida
+        Drica Bitarello me falou sobre sua capista: Sara Vertuan. Vendo as capas
+        que a Sara havia feito para os livros da Drica, não tive dúvidas de que
+        ela poderia entender exatamente o que eu precisava. Entrei em contato e
+        conversamos bastante sobre o que eu queria para essa capa. Sara além de
+        conversar comigo, também leu meu livro, para se envolver mais com a
+        história e conseguir captar toda essência.
+      </p>
+
+      <h1>GALERIA</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+
+      <h1>CONTATO</h1>
+      <h4>Gostou do que  viu? Entre em contato e vamos conversar!</h4>
+      <h5>CONFIRA MINHAS REDES SOCIAIS</h5>
+      <h6>Facebook</h6>
+      <h6>Instagram</h6>
+
+      <h3>AQUI VAI UM FORMULÁRIO</h3>
+      <h5>Nome</h5>
+      <h3>AQUI VAI UM CAMPO</h3>
+      <h5>E-mail</h5>
+      <h3>AQUI VAI UM CAMPO</h3>
+      <h5>Mensagem</h5>
+      <h3>AQUI VAI UM CAMPO</h3>
+
+      <h5>Enviar</h5>
+      <h3>AQUI VAI UM BOTÃO</h3>
+
+      <h3>AQUI VAI UM LINK P/ ORÇAMENTO</h3>
+    </>
   );
 }
 
