6de8bb11c53be643f7ff4ba1fb6b5b259f075aff
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-10-28 19:36:00 +0100

    AboutMe view creation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { Home } from 'views/Home';
+import { AboutMe } from 'views/AboutMe';
 import './index.css';
 
 function App() {
@@ -28,47 +29,7 @@ function App() {
 
       <Home />
 
-      <h1>SOBRE MIM</h1>
-      <h3>AQUI VAI UMA IMAGEM</h3>
-      <p>
-        Hello,
-        Meu nome é Sara Vertuan, sou Designer Editorial, desenvolvo Capas e
-        Diagramações de livros impressos e Ebooks, sou formada em Publicidade e
-        Propaganda. Sou freelancer para autores e editoras, meu amor pelos
-        livros faz com que o meu maior desejo seja o desenvolvimento de uma capa
-        linda que atraia os leitores e agrade meu cliente, sempre aperfeiçoando
-        meu trabalho para garantir a qualidade e melhorar cada dia mais.
-      </p>
-      <p>
-        Hello,
-        Mi chiamo Sara Vertuan, sono Editorial Designer, sviluppo copertine e
-        layout per libri stampati ed Ebook, sono laureata in Pubblicità e
-        Propaganda. Sono una freelance per autori ed editori, il mio amore per i
-        libri fa sì che il mio più grande desiderio sia lo sviluppo di una bella
-        copertina che attiri i lettori e faccia piacere al mio cliente,
-        migliorando sempre il mio lavoro per garantire la qualità e migliorare
-        ogni giorno.
-      </p>
-      <p>
-        My name is Sara Vertuan, I am Editorial Designer, I develop Covers and
-        Layouts for printed books and Ebooks, I am graduated in Advertising and
-        Publicity. I am a freelancer for authors and publishers, my love for
-        books makes my greatest desire is the development of a beautiful cover
-        that attracts readers and please my client, always improving my work to
-        ensure quality and improve every day.
-      </p>
-      <h5>Whatsapp</h5>
-      <h6>+5532261228</h6>
-      <h5>E-mail</h5>
-      <h6>saracvertuan@gmail.com</h6>
-      <h5>Facebook</h5>
-      <h6>facebook.com/designsaravertuan</h6>
-      <h5>Instagram</h5>
-      <h6>instagram.com/designersaravertuan</h6>
-      <h5>Behancé</h5>
-      <h6>behance.net/saravertuan</h6>
-      <h5>Linkedin</h5>
-      <h6>linkedin.com/in/saracvertuan/</h6>
+      <AboutMe />
 
       <h1>DEPOIMENTOS</h1>
       <h3>Gabriela Maya</h3>
