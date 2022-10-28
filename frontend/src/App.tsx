ffd88540374d09529f3de4a2cb1047f2a0c5d96d
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-10-28 19:40:17 +0100

    Contact view creation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Home } from 'views/Home';
 import { AboutMe } from 'views/AboutMe';
 import { Testimonies } from 'views/Testimonies';
 import { Gallery } from 'views/Gallery';
+import { Contact } from 'views/Contact';
 import './index.css';
 
 function App() {
@@ -37,24 +38,7 @@ function App() {
 
       <Gallery />
 
-      <h1>CONTATO</h1>
-      <h4>Gostou do que  viu? Entre em contato e vamos conversar!</h4>
-      <h5>CONFIRA MINHAS REDES SOCIAIS</h5>
-      <h6>Facebook</h6>
-      <h6>Instagram</h6>
-
-      <h3>AQUI VAI UM FORMULÁRIO</h3>
-      <h5>Nome</h5>
-      <h3>AQUI VAI UM CAMPO</h3>
-      <h5>E-mail</h5>
-      <h3>AQUI VAI UM CAMPO</h3>
-      <h5>Mensagem</h5>
-      <h3>AQUI VAI UM CAMPO</h3>
-
-      <h5>Enviar</h5>
-      <h3>AQUI VAI UM BOTÃO</h3>
-
-      <h3>AQUI VAI UM LINK P/ ORÇAMENTO</h3>
+      <Contact />
     </>
   );
 }
