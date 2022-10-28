97445274be16928819aaf4b063b474702d5bab2a
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-10-28 20:14:56 +0100

    Language component creation

---
+++ frontend/src/components/Language/index.tsx
@@ -0,0 +1,11 @@
+export function Language() {
+  return (
+    <>
+      <h1>LANGUAGE Component</h1>
+      <h3>Idioma</h3>
+      <h5>Português</h5>
+      <h5>Inglês</h5>
+      <h5>Italiano</h5>
+    </>
+  );
+};
