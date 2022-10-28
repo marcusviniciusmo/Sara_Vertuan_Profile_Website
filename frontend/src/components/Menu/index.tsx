ec089ec1ba523a5f315a83c4294aab0994a6a4dc
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-10-28 20:09:28 +0100

    Menu component creation

---
+++ frontend/src/components/Menu/index.tsx
@@ -0,0 +1,12 @@
+export function Menu() {
+  return (
+    <>
+      <h1>MENU Component</h1>
+      <h6>Início</h6>
+      <h6>Sobre Mim</h6>
+      <h6>Depoimentos</h6>
+      <h6>Galeria</h6>
+      <h6>Contato</h6>
+    </>
+  );
+};
--- frontend/src/components/Sidebar/index.tsx
+++ frontend/src/components/Sidebar/index.tsx
@@ -1,16 +1,14 @@
+import { Menu } from "components/Menu";
+
 export function Sidebar() {
   return (
     <>
       <h1>SIDEBAR Component</h1>
-      <h1>MENU</h1>
       <h3>AQUI VAI UMA IMAGEM</h3>
       <h4>Sara Vertuan</h4>
       <h5>Capas de livros e Diagramatização</h5>
-      <h6>Início</h6>
-      <h6>Sobre Mim</h6>
-      <h6>Depoimentos</h6>
-      <h6>Galeria</h6>
-      <h6>Contato</h6>
+      
+      <Menu />
     </>
   );
 };
