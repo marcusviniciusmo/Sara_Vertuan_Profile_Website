f4f4c31d33d28b84a89de6f39926b84c7fcc880b
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-10-28 20:25:22 +0100

    Navigating the screens through the menu

--- frontend/src/components/Menu/index.tsx
+++ frontend/src/components/Menu/index.tsx
@@ -1,12 +1,24 @@
+import { Link } from "react-router-dom"
+
 export function Menu() {
   return (
     <>
       <h1>MENU Component</h1>
-      <h6>Início</h6>
-      <h6>Sobre Mim</h6>
-      <h6>Depoimentos</h6>
-      <h6>Galeria</h6>
-      <h6>Contato</h6>
+      <Link to='/'>
+        <h6>Início</h6>
+      </Link>
+      <Link to='/aboutMe'>
+        <h6>Sobre Mim</h6>
+      </Link>
+      <Link to='/testimonies'>
+        <h6>Depoimentos</h6>
+      </Link>
+      <Link to='/gallery'>
+        <h6>Galeria</h6>
+      </Link>
+      <Link to='/contact'>
+        <h6>Contato</h6>
+      </Link>
     </>
   );
 };
