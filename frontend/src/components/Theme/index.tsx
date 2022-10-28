9cd73f09909b9511ff54d97c8401df3284af4d8e
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-10-28 21:46:20 +0100

    Creating the switch logic that controls the theme

--- frontend/src/components/Theme/index.tsx
+++ frontend/src/components/Theme/index.tsx
@@ -1,14 +1,25 @@
 import * as Switch from '@radix-ui/react-switch';
+import { useState } from 'react';
 import './styles.css';
 
 export function Theme() {
+  const [theme, setTheme] = useState<string>("Dark");
+
+  const handleTheme = () => {
+    setTheme(theme === 'Dark' ? "Light" : "Dark");
+  };
+
   return (
     <>
       <h1>THEME Component</h1>
       <h3>Tema</h3>
 
       <h5>Light</h5>
-      <Switch.Root className='switchRoot'>
+      <Switch.Root
+        className='switchRoot'
+        defaultChecked={theme === "Dark"}
+        onClick={handleTheme}
+      >
         <Switch.Thumb className='switchThumb' />
       </Switch.Root>
       <h5>Dark</h5>
