6dde980acb17445c8877d4a9bba83ffaa4497c50
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-10-29 20:03:24 +0100

    Adding icons to the Menu component

--- frontend/src/components/Menu/index.tsx
+++ frontend/src/components/Menu/index.tsx
@@ -1,22 +1,28 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { Home, Person, Chat, Collections, Call } from '@mui/icons-material';
 
 export function Menu() {
   return (
     <>
       <h1>MENU Component</h1>
       <Link to='/'>
+        <Home />
         <h6>Início</h6>
       </Link>
       <Link to='/aboutMe'>
+        <Person />
         <h6>Sobre Mim</h6>
       </Link>
       <Link to='/testimonies'>
+        <Chat />
         <h6>Depoimentos</h6>
       </Link>
       <Link to='/gallery'>
+        <Collections />
         <h6>Galeria</h6>
       </Link>
       <Link to='/contact'>
+        <Call />
         <h6>Contato</h6>
       </Link>
     </>
