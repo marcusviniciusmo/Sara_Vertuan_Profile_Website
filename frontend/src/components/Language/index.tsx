e250377825a0b66813dd6401f834e9716e30a481
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-10-29 10:04:38 +0100

    Adding the flags for the language

--- frontend/src/components/Language/index.tsx
+++ frontend/src/components/Language/index.tsx
@@ -1,10 +1,16 @@
+import { ReactCountryFlag } from 'react-country-flag';
+
 export function Language() {
   return (
     <>
       <h1>LANGUAGE Component</h1>
+
       <h3>Idioma</h3>
+      <ReactCountryFlag countryCode='BR' />
       <h5>Português</h5>
+      <ReactCountryFlag countryCode='GB' />
       <h5>Inglês</h5>
+      <ReactCountryFlag countryCode='IT' />
       <h5>Italiano</h5>
     </>
   );
