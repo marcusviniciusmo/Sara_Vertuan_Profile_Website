96ebc2522483e6a6c15ad75ab4f1397a51c2e54b
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-11-05 06:53:10 +0000

    Adding the icons to change the album photo

--- frontend/src/components/Album/index.tsx
+++ frontend/src/components/Album/index.tsx
@@ -1,14 +1,19 @@
+import { ArrowBackIos, ArrowForwardIos } from '@mui/icons-material';
 import { Section, AlbumPage } from './styles';
 
 export function Album() {
   const url = 'https://images.unsplash.com/photo-1596774468032-915cdd39ea39?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYzMjUxMjg1MQ&ixlib=rb-1.2.1&q=85';
 
   return (
-    <Section>
-      <div className='left next'></div>
-      <AlbumPage url={url} side='left' className='right next'></AlbumPage>
-      <AlbumPage side='right' className='left current'>TESTE</AlbumPage>
-      <div className='right current'></div>
-    </Section>
+    <>
+      <ArrowBackIos fontSize='large' />
+
+      <Section>
+        <AlbumPage url={url} side='left' className='right next'></AlbumPage>
+        <AlbumPage side='right' className='left current'>TESTE</AlbumPage>
+      </Section>
+
+      <ArrowForwardIos fontSize='large' />
+    </>
   );
 };
