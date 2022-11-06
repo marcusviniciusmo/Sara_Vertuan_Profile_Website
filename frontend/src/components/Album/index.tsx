446caa4e53d4c931cadf41d04419ef76d3c25320
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-11-06 20:20:36 +0000

    Album component styling according to language

--- frontend/src/components/Album/index.tsx
+++ frontend/src/components/Album/index.tsx
@@ -1,15 +1,17 @@
+/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react';
 import { useGlobalContext } from 'context';
 import { ArrowBackIos, ArrowForwardIos } from '@mui/icons-material';
 import { AlbumProps } from 'types/Album';
 import { MockedData } from 'mocks/Album';
-import { Container, Arrow, Section, Page } from './styles';
+import { Container, Arrow, Section, Page, Title, Info, Synopsis, Button } from './styles';
 import './styles.css';
 
 export function Album() {
   const [mockedData, setMockedData] = useState<AlbumProps>();
   const [index, setIndex] = useState<number>(0);
-  const { language } = useGlobalContext();
+  const { language, theme } = useGlobalContext();
 
   useEffect(() => {
     setMockedData(MockedData.find((data) => data.language === language));
@@ -25,7 +27,7 @@ export function Album() {
         setIndex(index - 1);
         previousPage?.classList.remove('flipLeft');
       }, 500);
-    };
+    }
   };
 
   const nextPage = () => {
@@ -42,16 +44,28 @@ export function Album() {
     };
   };
 
+  const showArrows = () => {
+    if (index <= 0) {
+      document.getElementById('arrowLeft')!.style.opacity = '0';
+      document.getElementById('arrowRight')!.style.opacity = '1';
+    }
+    else {
+      document.getElementById('arrowLeft')!.style.opacity = '1';
+      document.getElementById('arrowRight')!.style.opacity = '0';
+    }
+  };
+
+  useEffect(() => {
+    showArrows();
+  }, [index]);
+
   return (
     <Container>
-      {
-        index > 0 &&
-        <Arrow title='Previous Page'>
-          <ArrowBackIos fontSize='large' onClick={previousPage} />
-        </Arrow>
-      }
+      <Arrow title='Previous Page' id='arrowLeft'>
+        <ArrowBackIos fontSize='large' onClick={previousPage} />
+      </Arrow>
 
-      <Section>
+      <Section theme={theme}>
         <Page
           id='photoAlbumLeft'
           className='left'
@@ -64,16 +78,30 @@ export function Album() {
           className='right'
           side='right'
         >
-          {mockedData?.image[index].post}
+          <Title>{mockedData?.image[index].post}</Title>
+
+          <Info>{mockedData?.image[index].author}</Info>
+          <Info>{mockedData?.image[index].year}</Info>
+          <Info>{mockedData?.image[index].publisher}</Info>
+
+          <Synopsis>
+            <span>{mockedData?.image[index].synopsis}</span>
+            <Button
+              href={mockedData?.image[index].link}
+              className='noUnderline'
+              target={'_blank'}
+              theme={theme}
+            >
+              POST
+            </Button>
+          </Synopsis>
         </Page>
       </Section>
 
-      {
-        index + 1 < mockedData!?.image.length &&
-        <Arrow title='Next Page'>
-          <ArrowForwardIos fontSize='large' onClick={nextPage} />
-        </Arrow>
-      }
+
+      <Arrow title='Next Page' id='arrowRight'>
+        <ArrowForwardIos fontSize='large' onClick={nextPage} />
+      </Arrow>
     </Container>
   );
 };
--- frontend/src/components/Album/styles.tsx
+++ frontend/src/components/Album/styles.tsx
@@ -1,6 +1,10 @@
 import styled, { keyframes } from 'styled-components';
 
-interface AlbumPageProps {
+interface SectionProps {
+  theme: string;
+};
+
+interface PageProps {
   url?: string;
   side: string;
 };
@@ -30,14 +34,14 @@ export const Arrow = styled.div`
   cursor: pointer;
 `;
 
-export const Section = styled.section`
+export const Section = styled.section<SectionProps>`
   --duration: 0.5s;
   --ease-in-out: ease-in-out;
   
   width: 80%;
   height: 80vh;
   border-radius: 1vh;
-  box-shadow: 0 2vh 4vh rgba(0, 0, 0, 0.2);
+  box-shadow: 0 0.5rem 1rem var(--colorSecondary${(props) => props.theme});
   display: flex;
   perspective: 200rem;
   position: relative;
@@ -45,9 +49,8 @@ export const Section = styled.section`
   animation: ${zoom} calc(var(--duration) * 2) var(--ease-in-out);
 `;
 
-export const Page = styled.div<AlbumPageProps>`
-  background: url(${(props) => props.url && props.url});
-  background: ${(props) => props.url ? `url(${props.url})` : 'red'};
+export const Page = styled.div<PageProps>`
+  background: ${(props) => props.url ? `url(${props.url})` : `var(--background${props.theme})`};
   backface-visibility: hidden;
   background-attachment: fixed;
   background-position: center center;
@@ -62,4 +65,44 @@ export const Page = styled.div<AlbumPageProps>`
   transition-property: transform;
   transition-duration: var(--duration);
   transform-origin: ${(props) => props.side === 'left' ? '100% 50%' : '0% 50%'};
+`;
+
+export const Title = styled.strong`
+  margin-block: 4rem;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Info = styled.span`
+  margin-block: 1.5rem;
+  padding-inline: 3.5rem;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Synopsis = styled.div`
+  text-align: justify;
+  padding-inline: 3.5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 5rem;
+`;
+
+export const Button = styled.a`
+  background: var(--colorSecondary${(props) => props.theme});
+  color: var(--background${(props) => props.theme});
+  border-radius: 0.4rem;
+  width: 15rem;
+  height: 6rem;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    background: var(--colorPrimary${(props) => props.theme});
+    color: var(--background${(props) => props.theme});
+    border: 0.1rem solid var(--background${(props) => props.theme});
+    text-decoration: underline;
+  }
 `;
