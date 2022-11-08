1d02a444ae0f010003843d0d86fd882711c4fcc4
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-11-08 08:54:39 +0000

    Display of page numbers in the Album component

--- frontend/src/components/Album/index.tsx
+++ frontend/src/components/Album/index.tsx
@@ -16,106 +16,97 @@ export function Album() {
   const { language, theme } = useGlobalContext();
 
   const previousPage = () => {
-    if (index > 0) {
-      const previousPage = document.querySelector('#photoAlbumLeft');
+    const previousPage = document.querySelector('#photoAlbumLeft');
 
-      previousPage?.classList.add('flipLeft');
+    previousPage?.classList.add('flipLeft');
 
-      setTimeout(() => {
-        setIndex(index - 1);
-        previousPage?.classList.remove('flipLeft');
-      }, 500);
-    }
+    setTimeout(() => {
+      index > 0
+        ? setIndex(index - 1)
+        : setIndex(mockedData!?.image.length - 1);
+
+      previousPage?.classList.remove('flipLeft');
+    }, 500);
+
   };
 
   const nextPage = () => {
-    if (index + 1 < mockedData!?.image.length) {
+    const nextPage = document.querySelector('#photoAlbumRight');
 
-      const nextPage = document.querySelector('#photoAlbumRight');
+    nextPage?.classList.add('flipRight');
 
-      nextPage?.classList.add('flipRight');
+    setTimeout(() => {
+      index + 1 < mockedData!?.image.length
+        ? setIndex(index + 1)
+        : setIndex(0);
 
-      setTimeout(() => {
-        setIndex(index + 1);
-        nextPage?.classList.remove('flipRight');
-      }, 500);
-    };
+      nextPage?.classList.remove('flipRight');
+    }, 500);
+
   };
-
-  const showArrows = () => {
-    if (index <= 0) {
-      document.getElementById('arrowLeft')!.style.zIndex = '-1';
-      document.getElementById('arrowRight')!.style.zIndex = '0';
-    } else if (index + 1 >= mockedData!?.image.length) {
-      document.getElementById('arrowRight')!.style.zIndex = '-1';
-    } else {
-      document.getElementById('arrowLeft')!.style.zIndex = '0';
-    }
-  };
-
-  useEffect(() => {
-    showArrows();
-  }, [index]);
 
   useEffect(() => {
     setMockedData(MockedData.find((data) => data.language === language));
   }, [language]);
 
   return (
-    <Container>
-      <Arrow title='Previous Page' id='arrowLeft'>
-        <ArrowBackIos fontSize='large' onClick={previousPage} />
-      </Arrow>
+    <div id='albumComponent'>
+      <span>{`${index + 1} / ${mockedData?.image.length}`}</span>
+      <Container>
+        <Arrow title='Previous Page' id='arrowLeft'>
+          <ArrowBackIos fontSize='large' onClick={previousPage} />
+        </Arrow>
 
-      <Section theme={theme}>
-        <Page
-          id='photoAlbumLeft'
-          className='left'
-          side='left'
-          url={mockedData?.image[index].url}
-        />
+        <Section theme={theme}>
+          <Page
+            id='photoAlbumLeft'
+            className='left'
+            side='left'
+            url={mockedData?.image[index].url}
+          />
 
-        <Page
-          id='photoAlbumRight'
-          className='right'
-          side='right'
-        >
-          <Title>{mockedData?.image[index].title}</Title>
+          <Page
+            id='photoAlbumRight'
+            className='right'
+            side='right'
+          >
+            <Title>{mockedData?.image[index].title}</Title>
 
-          <Info>{mockedData?.image[index].author}</Info>
+            <Info>{mockedData?.image[index].author}</Info>
 
-          <Info>
-            {`${mockedData?.image[index].publisher} - 
+            <Info>
+              {`${mockedData?.image[index].publisher} - 
             ${mockedData?.image[index].year}`}
-          </Info>
+            </Info>
 
-          <ButtonsArea>
-            <Dialog.Root>
-              <Dialog.Trigger className='dialogTrigger'>
-                <Button theme={theme}>
-                  SINOPSE
+            <ButtonsArea>
+              <Dialog.Root>
+                <Dialog.Trigger className='dialogTrigger'>
+                  <Button theme={theme}>
+                    SINOPSE
+                  </Button>
+                </Dialog.Trigger>
+
+                <Button
+                  href={mockedData?.image[index].link}
+                  className='noUnderline'
+                  target={'_blank'}
+                  theme={theme}
+                >
+                  Check post
                 </Button>
-              </Dialog.Trigger>
 
-              <Button
-                href={mockedData?.image[index].link}
-                className='noUnderline'
-                target={'_blank'}
-                theme={theme}
-              >
-                Check post
-              </Button>
-
-              <Synopsis synopsis={mockedData!?.image[index].synopsis} />
-            </Dialog.Root>
-          </ButtonsArea>
-        </Page>
-      </Section>
+                <Synopsis synopsis={mockedData!?.image[index].synopsis} />
+              </Dialog.Root>
+            </ButtonsArea>
+          </Page>
+        </Section>
 
 
-      <Arrow title='Next Page' id='arrowRight'>
-        <ArrowForwardIos fontSize='large' onClick={nextPage} />
-      </Arrow>
-    </Container>
+        <Arrow title='Next Page' id='arrowRight'>
+          <ArrowForwardIos fontSize='large' onClick={nextPage} />
+        </Arrow>
+      </Container>
+    </div>
   );
 };
