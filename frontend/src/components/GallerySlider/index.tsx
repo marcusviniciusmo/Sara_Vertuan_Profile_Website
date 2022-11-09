149d582ef1f96378c4a93a4d1e15cb03fe601026
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-11-09 07:42:06 +0000

    GallerySlider component creation

---
+++ frontend/src/components/GallerySlider/index.tsx
@@ -0,0 +1,7 @@
+export function GallerySlider() {
+  return (
+    <>
+      Gallery Slider
+    </>
+  );
+};
--- frontend/src/views/Gallery/index.tsx
+++ frontend/src/views/Gallery/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "components/Header";
 import { Sidebar } from "components/Sidebar"
+import { GallerySlider } from "components/GallerySlider";
 import { Interface } from "styles/Interface";
 
 export function Gallery() {
@@ -11,6 +12,7 @@ export function Gallery() {
 
       <Interface>
         <h1>GALERIA View</h1>
+        <GallerySlider />
         <h3>AQUI VAI UMA IMAGEM</h3>
         <h3>AQUI VAI UMA IMAGEM</h3>
         <h3>AQUI VAI UMA IMAGEM</h3>
